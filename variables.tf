06f7239626b751373a631bbc3e53d3984e6c114d
Jeff Miller <jeff.miller@fullbay.com>
2026-01-15 09:24:07 -0600

    Update variables to change some values

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "jeff-poc-metadata"
+  default     = "jeff-partdeux-meta"
 }
 
 variable "environment" {
