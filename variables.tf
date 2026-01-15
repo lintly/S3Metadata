a93ac33619f84dfdbca34ae026325873a10c2b0b
Jeff Miller <jeff.miller@fullbay.com>
2026-01-15 09:49:51 -0600

    Remove Athena and Glue

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
-  default     = "jeff-partdeux-meta"
+  default     = "jeff-basic-meta-test"
 }
 
 variable "environment" {
